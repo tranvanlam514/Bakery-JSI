7999f681c0e8d02e4a4f7d0543a57ff77d8508a9
tranvanlam514 <tranlam06072011@gmail.com>
2025-05-10 17:45:51 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAswlEROqESGvRJrOgNK6PLiyYQEI-FQPk",
+    authDomain: "spck-jsi-3e996.firebaseapp.com",
+    projectId: "spck-jsi-3e996",
+    storageBucket: "spck-jsi-3e996.firebasestorage.app",
+    messagingSenderId: "31009511475",
+    appId: "1:31009511475:web:83bb7453c94790f6a5ab46",
+    measurementId: "G-N94X8S07W3"
+};
